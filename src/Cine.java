bd0d9509dafb700465f4c7119fd62f23d7252423
angelantelo <angelantelodeanta@gmail.com>
2024-02-20 12:34:28 +0100

    Este es el commit con la clase Cine terminada con su documentación

--- src/Cine.java
+++ src/Cine.java
@@ -13,4 +13,37 @@ public class Cine {
         this.aforo = aforo;
         this.salas = salas;
     }
+    /*
+    * He creado una opción para mostrar la Información sobre las Salas y la relación con el Cine.
+    * */
+    public void mostrarInformacionSalas() {
+        System.out.println("Información de las salas del cine " + nombre + ":");
+        for (Sala sala : salas) {
+            System.out.println("Sala " + sala.getNumeroSala() + ": " + sala.getPelicula().getTitulo());
+        }
+    }
+    /*
+    * Esta opción es para ver el Estado de la Sala en relación con el Cine.
+    * */
+    public void mostrarEstadoSala(int numeroSala) {
+        for (Sala sala : salas) {
+            if (sala.getNumeroSala() == numeroSala) {
+                sala.mostrarEstadoButacas();
+                return;
+            }
+        }
+        System.out.println("La sala especificada no existe.");
+    }
+    /*
+    * Y esta es la opción creada para reservar un Asiento.
+    * */
+    public boolean reservarAsiento(int numeroSala, int fila, int columna, String email) {
+        for (Sala sala : salas) {
+            if (sala.getNumeroSala() == numeroSala) {
+                return sala.reservarAsiento(fila, columna, email);
+            }
+        }
+        System.out.println("La sala especificada no existe.");
+        return false;
+    }
 }
